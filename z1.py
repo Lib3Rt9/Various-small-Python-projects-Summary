f45e8214173eb0b6fe639a7d80bdf48f8d6a6866
Lib3Rt9 <phamgiaphuc011709@gmail.com>
2023-04-12 00:24:25 +0700

    add input prefix and starting points

--- z1.py
+++ z1.py
@@ -1,11 +1,15 @@
 import os
 import pathlib
+import glob
+from itertools import chain
 
 path = pathlib.Path(__file__).parent.resolve()
-COUNT = 668
 
-# print(path)
-# print("123")
+
+PREFIX = input("Enter a prefix: ")
+COUNT = int(input("Enter starting number: "))
+
+print(path)
 
 # place the all willing-to-rename files and this z1.py file into the same folder
 os.chdir(path)                # change this path to where this file is located
@@ -16,9 +20,13 @@ def increment():
     global COUNT
     COUNT = COUNT + 1
 
-for f in os.listdir():
-    f_name, f_ext = os.path.splitext(f)
-    f_name = "1" + str(COUNT)                 # "000" is the prefix, change to whatever is ok, then the suffix is the number incresing from the COUNT in line 6
+for file in os.listdir():
+
+
+    file_name, file_ext = os.path.splitext(file)
+    file_name = PREFIX + str(COUNT)                 # "000" is the prefix, change to whatever is ok, then the suffix is the number incresing from the COUNT in line 6
+    
     increment()
-    new_name = '{}{}'.format(f_name, f_ext)
-    os.rename(f, new_name)
+    
+    new_name = '{}{}'.format(file_name, file_ext)
+    os.rename(file, new_name)
