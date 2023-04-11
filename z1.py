46f5c6fb5bdbed92e5215f07b8ae73c7a360b179
Lib3Rt9 <phamgiaphuc011709@gmail.com>
2023-04-11 18:55:09 +0700

    Add auto get path of current directory

--- z1.py
+++ z1.py
@@ -1,18 +1,24 @@
 import os
+import pathlib
+
+path = pathlib.Path(__file__).parent.resolve()
+COUNT = 668
+
+# print(path)
+# print("123")
 
 # place the all willing-to-rename files and this z1.py file into the same folder
-os.chdir('D:\RenameMultiFile')                # change this path to where this file is located
+os.chdir(path)                # change this path to where this file is located
 print(os.getcwd())
-COUNT = 0
 
 #function to increment count to make the files sorted0
 def increment():
-  global COUNT
-  COUNT = COUNT + 1
+    global COUNT
+    COUNT = COUNT + 1
 
 for f in os.listdir():
-  f_name, f_ext = os.path.splitext(f)
-  f_name = "000" + str(COUNT)                 # "000" is the prefix, change to whatever is ok, then the suffix is the number incresing from the COUNT in line 6
-  increment()
-  new_name = '{} {}'.format(f_name, f_ext)
-  os.rename(f, new_name)
+    f_name, f_ext = os.path.splitext(f)
+    f_name = "1" + str(COUNT)                 # "000" is the prefix, change to whatever is ok, then the suffix is the number incresing from the COUNT in line 6
+    increment()
+    new_name = '{}{}'.format(f_name, f_ext)
+    os.rename(f, new_name)
